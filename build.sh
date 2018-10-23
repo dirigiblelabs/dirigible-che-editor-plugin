fd7db026b6bf31e0ca281b01ebf252182836dfee
Nedelcho Delchev <delchevn@gmail.com>
2018-10-23 14:00:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 set -e
 set -u
 
-if [ -f che-editor-plugin.tar.gz ]; then
-    rm che-editor-plugin.tar.gz
+if ! [ -d assembly ]; then
+    mkdir assembly
+fi
+
+if [ -f assembly/dirigible-che-editor-plugin.tar.gz ]; then
+    rm assembly/dirigible-che-editor-plugin.tar.gz
 fi
 
 cd etc
-tar zcvf ../che-editor-plugin.tar.gz .
+tar zcvf ../assembly/dirigible-che-editor-plugin.tar.gz .
 
